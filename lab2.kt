0870f58eb89e044b02bbcc70b89b97d96f753882
shhhrk <execezeli@gmail.com>
2025-03-06 14:03:43 +0300

    Create lab2.kt
    Basic types
    HelloWorld2:Вывод ФИО, возраст , группa.

---
+++ lab2.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val name = "Иван"
+    val surname = "Иванов"
+    val age = 20
+    val group= "PS21"
+    print("Меня зовут $name $surname, мой возраст $age я учусь в группе $group")
+}
