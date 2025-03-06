e16c5cc788d50ae64316ac0afa9dae5920e12556
shhhrk <execezeli@gmail.com>
2025-03-06 14:15:30 +0300

    Create lab11.kt
    Функции(3)

---
+++ lab11.kt
@@ -0,0 +1,9 @@
+fun intervalInSeconds(hours: Int=0, minutes: Int=0, seconds: Int=0) =
+    ((hours * 60) + minutes) * 60 + seconds
+
+fun main() {
+    println(intervalInSeconds(1, 20, 15))
+    println(intervalInSeconds(0, 1, 25))
+    println(intervalInSeconds(2, 0, 0))
+    println(intervalInSeconds(0, 10, 0))
+    println(intervalInSeconds(1, 0, 1))
