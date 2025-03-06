f43bfed40188fe1de39a960087df9bc4ed31e879
shhhrk <execezeli@gmail.com>
2025-03-06 14:11:31 +0300

    Create lab7.kt
    Control flow(2)

---
+++ lab7.kt
@@ -0,0 +1,8 @@
+fun main() {
+      var pizzaSlices = 0
+    while(pizzaSlices<8){
+        pizzaSlices++
+        println("У нас $pizzaSlices кусочков пиццы :(")
+    }
+    println("У нас $pizzaSlices кусочков пиццы. Ура! У нас есть целая пицца! :D")
+}
