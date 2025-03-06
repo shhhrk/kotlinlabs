b3d1af14f1399b33010960f03d8bb57ac31cbf0c
shhhrk <execezeli@gmail.com>
2025-03-06 14:08:53 +0300

    Create lab4.kt
    Collections(2)
    Collections CO1: У вас есть список «зеленых» номеров и список «красных» номеров.
    
    Завершите код, чтобы вывести общее количество  всех номеров.

---
+++ lab4.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val fruits = listOf("apple", "avocado", "orange")
+    val vegetables = listOf("potato", "tomato")
+    print("Количество всех ${fruits.count()+vegetables.count()}")
+  
+}
