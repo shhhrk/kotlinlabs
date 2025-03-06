b19311bc7aca46920f456a17e79154de61c62731
shhhrk <execezeli@gmail.com>
2025-03-06 15:01:16 +0300

    Update lab14.kt

--- lab14.kt
+++ lab14.kt
@@ -1,14 +1,19 @@
-import kotlin.random.Random
-
 data class Employee(val name: String, var salary: Int)
-class RandomEmployeeGenerator(_name: String, _minsalary: Int, _maxsalary: Int)
-// Write your code here
+class RandomEmployeeGenerator(var minSalary: Int, var maxSalary: Int) {
+    val names = listOf("Melanie","Max","Eugune","Martey","Clint")
+    fun generateEmployee(): Employee {
+        val name = names.random()
+        val salary = Random.nextInt(minSalary, maxSalary + 1)
+        return Employee(name, salary)
+    }
+}
 
 fun main() {
     val empGen = RandomEmployeeGenerator(10, 30)
     println(empGen.generateEmployee())
     println(empGen.generateEmployee())
     println(empGen.generateEmployee())
+    
     empGen.minSalary = 50
     empGen.maxSalary = 100
     println(empGen.generateEmployee())
